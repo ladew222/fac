de0d8e275162cb48d435d8fed5ccd1e520576524
Martha A Boehm <maboehm@RC152-DPJJ9.local>
2022-06-27 14:14:34 -0500

    new file folder structure

---
+++ 2022-23 Programs/js/main.js
@@ -0,0 +1,42 @@
+ var checkbox = document.querySelector('input[name=mode]');
+
+        checkbox.addEventListener('change', function() {
+            if(this.checked) {
+                trans()
+                document.documentElement.setAttribute('data-theme', 'dark')
+            } else {
+                trans()
+                document.documentElement.setAttribute('data-theme', 'light')
+            }
+        })
+
+        let trans = () => {
+            document.documentElement.classList.add('transition');
+            window.setTimeout(() => {
+                document.documentElement.classList.remove('transition');
+            }, 1000)
+        }
+		
+		
+$(document).ready(function() {
+    $('input[type=checkbox][name=mode]').change(function() {
+        if ($(this).prop("checked")) {
+            //alert(`${this.value} is checked`);
+			$(document.documentElement).attr("data-theme","dark");
+        }
+        else {
+            //alert(`${this.value} is unchecked`);
+			$(document.documentElement).attr("data-theme","light");
+        }
+    });
+	
+	$('.clickableList').click(function() {
+		//event.preventDefault();
+		
+	});
+	$('.close').click(function() {
+		//event.preventDefault();
+		$(".navbar").removeClass("padded");
+		
+	});
+});
